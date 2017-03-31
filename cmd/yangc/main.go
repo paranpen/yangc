2b70c16a51e0eea678fbdd8261a0ed759662895a
ParanPen <twkim98@gmail.com>
2017-03-31 16:03:59 +0900

    Update main.go

--- cmd/yangc/main.go
+++ cmd/yangc/main.go
@@ -2,17 +2,88 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"sort"
 
+	"github.com/openconfig/goyang/pkg/yang"
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/cobra/cmd"
 )
 
-var rootCmd = &cobra.Command{Use: "yangc"}
+var mainCmd = &cobra.Command{
+	Use:   os.Args[0],
+	Short: "Tool to translate Yang Models to Unit Data API",
+}
+
+var yangFileName string
+
+func init() {
+	mainCmd.PersistentFlags().StringVarP(&yangFileName, "file", "f", "test.yang", "yang file name")
+}
 
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	if _, err := mainCmd.ExecuteC(); err != nil {
 		os.Exit(-1)
 	}
 }
+
+func doCompile(fileName string) []*yang.Entry {
+	var entries []*yang.Entry
+
+	ms := yang.NewModules()
+	files := make([]string, 0, 10)
+	files = append(files, fileName)
+
+	if len(files) == 0 {
+		data, err := ioutil.ReadAll(os.Stdin)
+		if err == nil {
+			err = ms.Parse(string(data), "<STDIN>")
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+	for _, name := range files {
+		if err := ms.Read(name); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+	}
+
+	// Process the read files, exiting if any errors were found.
+	exitIfError(ms.Process())
+
+	// Keep track of the top level modules we read in.
+	// Those are the only modules we want to print below.
+	mods := map[string]*yang.Module{}
+	var names []string
+
+	for _, m := range ms.Modules {
+		if mods[m.Name] == nil {
+			mods[m.Name] = m
+			names = append(names, m.Name)
+		}
+	}
+	sort.Strings(names)
+	entries = make([]*yang.Entry, len(names))
+	for x, n := range names {
+		fmt.Printf("- module[%s] ...\n", n)
+		entries[x] = yang.ToEntry(mods[n])
+		// copy Typedef dictionary & return?
+	}
+	return entries
+}
+
+// exitIfError writes errs to standard error and exits with an exit status of 1.
+// If errs is empty then exitIfError does nothing and simply returns.
+func exitIfError(errs []error) {
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
+
