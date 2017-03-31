054cc1c7f1af052f71bae76ffb3d757caf8f62ec
ParanPen <twkim98@gmail.com>
2017-03-31 16:04:28 +0900

    Create header.go

---
+++ cmd/yangc/header.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/openconfig/goyang/pkg/yang"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var headerCmd = &cobra.Command{
+		Use:   ".",
+		Short: "yangc to generate C header files",
+		Run: func(cmd *cobra.Command, args []string) {
+			entries := doCompile(yangFileName)
+			doHeader(os.Stdout, entries)
+		},
+	}
+	mainCmd.AddCommand(headerCmd)
+}
+
+func doHeader(w io.Writer, entries []*yang.Entry) {
+	// do stuff
+	tds := yang.TypeDict.Typedefs()
+	printTypedefs(tds) // look up by nodes?
+}
+
+// Print type dictionary (by twkim)
+func printTypedefs(tds []*yang.Typedef) {
+	for _, v := range tds {
+		// case enumeration
+		kind := (*v.YangType).Kind
+		fmt.Printf("%s %s {\n", kind, v.Name)
+		fmt.Printf("}\n")
+	}
+}
